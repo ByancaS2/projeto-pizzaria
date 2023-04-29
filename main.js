ab78ec4d337e587c4c1a50803a92c582096672b8
Byazinhah <120607803+ByancaS2@users.noreply.github.com>
2023-04-29 20:27:42 -0300

    Update main.js

--- main.js
+++ main.js
@@ -19,14 +19,14 @@ document.getElementById("displayMenu").innerHTML = htmldata;
 
 function addItem(){
 var htmldata;
-  var imgtags='<img id="im1" src="images/pizzaImg.png"/>';
+var imgtags='<img id="im1" src="images/pizzaImg.png"/>'
 var item=document.getElementById("addItem").value;
 menuListArray.sort();
 htmldata="";
 for (var i=0;i<menuListArray.length; i++){
-    htmldata=htmldata+ menuListArray[i] + '<br>';
+    htmldata=htmldata+imgtags+ menuListArray[i]+'<br>';
 }
-    document.getElementById("displayMenu").innerHTML = htmldata;
+    document.getElementById("displayAddedMenu").innerHTML = htmldata;
 }
 
 function addTop(){
